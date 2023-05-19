d1981f84901b45cb7649ee0122fc2738b63afd72
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2023-05-19 03:10:53 -0700

    addNode

--- SingleLinkedList_167/SingleLinkedList_167.cpp
+++ SingleLinkedList_167/SingleLinkedList_167.cpp
@@ -9,3 +9,28 @@ struct Node {
 
 Node* START = NULL;
 
+void addNode() {
+	int nim;
+	string nama;
+	Node* nodeBaru = new Node();
+	cout << "Masukan NIM: ";
+	cin >> nim;
+	cout << "Masukan Nama: ";
+	cin >> nama;
+	nodeBaru->noMhs = nim;
+	nodeBaru->name = nama;
+
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+
+		nodeBaru->next = START;
+		START = nodeBaru;
+		return;
+	}
+
+	
+
