dff11f0249ae178b7a0edb94cedf8cfc4100fe3c
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2023-05-19 03:54:14 -0700

    switch/pilihan

--- SingleLinkedList_167/SingleLinkedList_167.cpp
+++ SingleLinkedList_167/SingleLinkedList_167.cpp
@@ -166,4 +166,30 @@ int main() {
 				system("pause");
 				system("cls");
 				break;
+			case 2:
+				if (listEmpty())
+				{
+					cout << "List kosong" << endl;
+					system("pause");
+					system("cls");
+					break;
+				}
+
+				int nim;
+				cout << "Masukan NIM: ";
+				cin >> nim;
+				if (deleteNode(nim)) {
+					cout << "nim: " << nim << " berhasil dihapus" << endl;
+					system("pause");
+					system("cls");
+				}
+				else
+					cout << "Data tidak ditemukan" << endl;
+				break;
+			case 3:
+				traverse();
+				break;
+			case 4:
+				searchData();
+				break;
 			
